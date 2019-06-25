7b5499ee6536063d4df7e25c2cf4253a8531d8be
Maria Cross <mscross92@googlemail.com>
2019-06-25 23:26:55 +0100

    Update HardNetHPatchesSplits.py

--- code/HardNetHPatchesSplits.py
+++ code/HardNetHPatchesSplits.py
@@ -662,7 +662,7 @@ def main(train_loader, test_loaders, model, logger, file_logger):
         # iterate over test loaders and test results
         #train_loader, test_loaders2 = create_loaders(load_random_triplets=triplet_flag)
         train(train_loader, model, optimizer1, epoch, logger, triplet_flag)
-        for test_loader in test_loaders:
+        # for test_loader in test_loaders:
             # test(test_loader['dataloader'], model, epoch, logger, test_loader['name'])
         if TEST_ON_W1BS:
             # print(weights_path)
