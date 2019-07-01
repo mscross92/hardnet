c611ed8809247d12a95ef4d919be82cfc60040bc
Maria Cross <mscross92@googlemail.com>
2019-07-01 19:45:39 +0100

    Update Losses.py

--- code/Losses.py
+++ code/Losses.py
@@ -197,16 +197,20 @@ def loss_semi_hard(anchor, positive, visualise_idx, anchor_swap = False, anchor_
             cat_d = torch.cat((dist_matrix_a,dist_matrix_p),1)
             cat_mins = torch.cat([mn.unsqueeze(-1)]*(len(anchor) + len(positive)),1)
             del mn
-            inc_negs = torch.le((torch.gt(cat_d,cat_mins)),torch.add(cat_mins.byte(), 0.3))
+            inc_negs = torch.le((torch.gt(cat_d,cat_mins)),torch.add(cat_mins.byte(), 0.5))
 
             # randomly select a negative distance for each row
             valid_idx = inc_negs.nonzero()
             unique_rows = valid_idx[:, 0].unique()
             valid_row_idx = [valid_idx[valid_idx[:, 0] == u] for u in unique_rows]
             ret = []
-            for v in valid_row_idx:
-                choice = torch.multinomial(torch.arange(v.size(0)).float(), 1)
-                ret.append(inc_negs[v[choice].squeeze().chunk(2)])
+            for ii,v in enumerate(valid_row_idx):
+                if v.size(0)>0:
+                    choice = torch.multinomial(torch.arange(v.size(0)).float(), 1)
+                    ret.append(inc_negs[v[choice].squeeze().chunk(2)])
+                else: # if none available in range, set loss as that of hard negative
+                    print('no negative index')
+                    ret.append(min_neg[ii])
             min_neg = torch.stack(ret).type(torch.cuda.FloatTensor)
             
 
