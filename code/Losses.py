f0c4dbc9d7c5fa2d602297b376668b371a98787b
Maria Cross <mscross92@googlemail.com>
2019-07-05 14:03:26 +0100

    Update Losses.py

--- code/Losses.py
+++ code/Losses.py
@@ -221,7 +221,7 @@ def loss_semi_hard(anchor, positive, visualise_idx, anchor_swap = False, anchor_
             ret = []
             for ii in range(len(anchor)):
                 valid_dists = inc_negs[ii].nonzero().cpu().numpy().astype('float64')
-                print(len(valid_dists))
+                print(valid_dists.shape)
                 if len(valid_dists)>0:
                     d = np.random.choice(valid_dists)
                 else:
