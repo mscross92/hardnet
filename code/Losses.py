913da865ace505e5193a86f6f04f9cefc7e94668
Maria Cross <mscross92@googlemail.com>
2019-07-05 13:29:30 +0100

    Update Losses.py

--- code/Losses.py
+++ code/Losses.py
@@ -220,6 +220,7 @@ def loss_semi_hard(anchor, positive, visualise_idx, anchor_swap = False, anchor_
 
             # randomly select a negative distance for each row
             valid_idx = inc_negs.nonzero()
+            print(valid_idx.shape)
             unique_rows = valid_idx[:, 0].unique()
             valid_row_idx = [valid_idx[valid_idx[:, 0] == u] for u in unique_rows]
             ret = []
