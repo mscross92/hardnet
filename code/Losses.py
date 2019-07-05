a5a61bfcd33389105f5b359330cd3e7c3012028f
Maria Cross <mscross92@googlemail.com>
2019-07-05 11:16:59 +0100

    Update Losses.py

--- code/Losses.py
+++ code/Losses.py
@@ -221,7 +221,7 @@ def loss_semi_hard(anchor, positive, visualise_idx, anchor_swap = False, anchor_
             
             # get row 
             dist_row = inc_negs[visualise_idx]
-            n_dist = float(min_neg[visualise_idx])
+            n_dist = float(min_neg[visualise_idx].cpu())
             n_idx = np.where(dist_row.numpy().astype('float64') == n_dist)
             if n_idx<len(anchor):
                 n_type = 0
