09d93f0e60db87981c7c9f1bb4364eeb16b7a4ac
Maria Cross <mscross92@googlemail.com>
2019-07-15 21:02:07 +0100

    Update HardNetHPatchesSplits.py

--- code/HardNetHPatchesSplits.py
+++ code/HardNetHPatchesSplits.py
@@ -1094,7 +1094,7 @@ def test(test_loader, model, epoch, logger, logger_test_name, test_sample_x, tes
     # plot against image label
     fig, ax = plt.subplots(figsize=(6,6))
     plt.plot(all_val_set_images,dist_m_all_val,'.',color=(0.0, 0.5, 0.0, 0.4))
-    plt.plot(average_scores,img_y_labels,'o-', color='orange',label='Mean distance')
+    plt.plot(img_y_labels,average_scores,'o-', color='orange',label='Mean distance')
     # plt.axhline(thresh,linewidth=1, color='k', linestyle='--',label='Threshold')
     plt.xlabel('Image index')
     plt.ylabel('distance')
@@ -1156,7 +1156,7 @@ def test(test_loader, model, epoch, logger, logger_test_name, test_sample_x, tes
     fig, ax = plt.subplots(figsize=(6,6))
     plt.plot(sample_train_set_images,dist_m_all_val,'.',color=(0.0, 0.5, 0.0, 0.4))
     # plt.plot(average_scores,'o',label='mean distance for image')
-    plt.plot(average_scores,img_y_labels,'o-', color='orange',label='Mean distance')
+    plt.plot(img_y_labels,average_scores,'o-', color='orange',label='Mean distance')
     plt.xlabel('Image index')
     plt.ylabel('distance')
     # plt.legend()
