112cb1e0210a1a2e9947ce6e56f33a3d5a568150
Maria Cross <mscross92@googlemail.com>
2019-07-15 23:06:18 +0100

    Update HardNetHPatchesSplits.py

--- code/HardNetHPatchesSplits.py
+++ code/HardNetHPatchesSplits.py
@@ -1080,11 +1080,11 @@ def test(test_loader, model, epoch, logger, logger_test_name, test_sample_x, tes
         # compute average for each image
         average_scores = []
         dist_m_all_val = np.array(dist_m_all_val)
-        sample_train_set_images = np.array(sample_train_set_images)
-        print(len(sample_train_set_images))
+        all_val_set_images = np.array(all_val_set_images)
+        print(len(all_val_set_images))
         print(len(dist_m_all_val))
         for ly in img_y_labels:
-            in_l = np.where(sample_train_set_images==ly)
+            in_l = np.where(all_val_set_images==ly)
             av = np.median(dist_m_all_val[in_l])
             average_scores.append(av)
         print(len(average_scores))
