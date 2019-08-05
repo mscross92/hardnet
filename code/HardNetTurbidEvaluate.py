25ef93ea694c2f91d1b851081dcc6c191f98086b
Maria Cross <mscross92@googlemail.com>
2019-08-05 13:41:36 +0100

    Update HardNetTurbidEvaluate.py

--- code/HardNetTurbidEvaluate.py
+++ code/HardNetTurbidEvaluate.py
@@ -1419,6 +1419,7 @@ def main(train_loader, test_loader, model, logger, file_logger):
     for p in comparisons:
         pos_flag = p[4]
         d_idx_a = label_indices[p[1],p[0]]
+        print(d_idx_a)
         desc_a = desc_xv[d_idx_a].cpu().numpy()
 
         d_idx_b = label_indices[p[3],p[2]]
