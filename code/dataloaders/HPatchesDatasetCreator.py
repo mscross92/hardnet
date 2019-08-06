bb8c2d16f4295658d4fa4add26d8eb43af884f32
Maria Cross <mscross92@googlemail.com>
2019-08-06 23:44:09 +0100

    update set labels

--- code/dataloaders/HPatchesDatasetCreator.py
+++ code/dataloaders/HPatchesDatasetCreator.py
@@ -12,8 +12,8 @@ types = ['0','1','2','3','4','5','6','7','8','9']
 #        '11','12','13','14','15','16','17','18']
 
 # types = ['0','1','2']
-# splits = ['a']
-splits = ['b']
+splits = ['a']
+# splits = ['b']
 
 #images_to_exclude = ['v_adam', 'v_boat', 'v_graffiti', 'v_there','i_dome']
 
