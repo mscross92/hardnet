686b7b0d0331d1719dcbf3df9a2dbe776600537b
Maria Cross <mscross92@googlemail.com>
2019-08-07 01:39:36 +0100

    16x16

--- code/HardNetHPatchesSplits.py
+++ code/HardNetHPatchesSplits.py
@@ -468,7 +468,7 @@ class HardNet(nn.Module):
             nn.Conv2d(32, 32, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(32, affine=False),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(64, affine=False),
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1, bias=False),
