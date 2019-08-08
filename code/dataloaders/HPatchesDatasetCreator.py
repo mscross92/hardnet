a119b0c5d984c0d9ceccf5517e1469667012dd93
Maria Cross <mscross92@googlemail.com>
2019-08-08 15:35:51 +0100

    32x32 patches

--- code/dataloaders/HPatchesDatasetCreator.py
+++ code/dataloaders/HPatchesDatasetCreator.py
@@ -48,7 +48,7 @@ class HPatches(data.Dataset):
                 n_patches = int(h / w)
                 for i in range(n_patches):
                     patch = image[i * (w): (i + 1) * (w), 0:w]
-                    patch = cv2.resize(patch, (64, 64))
+                    patch = cv2.resize(patch, (32, 32))
                     patch = np.array(patch, dtype=np.uint8)
                     patches.append(patch)
                     labels.append(i+counter)
@@ -75,7 +75,7 @@ class HPatches(data.Dataset):
                 n_patches = int(h / w)
                 for i in range(n_patches):
                     patch = image[i * (w): (i + 1) * (w), 0:w]
-                    patch = cv2.resize(patch, (64, 64))
+                    patch = cv2.resize(patch, (32, 32))
                     patch = np.array(patch, dtype=np.uint8)
                     patches.append(patch)
                     labels.append(i+counter)
