98fbe7c4a5fc70516db88fe4a374599e03d29bd9
Maria Cross <mscross92@googlemail.com>
2019-08-19 14:34:28 +0100

    Update HardNetTurbidEvaluate.py

--- code/HardNetTurbidEvaluate.py
+++ code/HardNetTurbidEvaluate.py
@@ -1291,7 +1291,7 @@ def main(train_loader, test_loader, model, logger, file_logger):
             files = sorted(files)
             for file in files:
                 if yy != patch_dir:
-                    print(yy)
+                    # print(yy)
                     s = file.replace('.jpg','')
                     print(s)
                     s = s[1:]
