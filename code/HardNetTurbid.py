5210304d5e00ce2a1370e2b8d1ece26f5caa91d8
Maria Cross <mscross92@googlemail.com>
2019-10-03 12:49:03 +0100

    tfeat

--- code/HardNetTurbid.py
+++ code/HardNetTurbid.py
@@ -320,6 +320,29 @@ class TurbidDatasetsLoader(data.Dataset):
             # if self.train:
             return self.triplets.size(0)
 
+class TNet(nn.Module):
+    """TFeat model definition
+    """
+    def __init__(self):
+        super(TNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.InstanceNorm2d(1, affine=False),
+            nn.Conv2d(1, 32, kernel_size=7),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=6),
+            nn.Tanh()
+        )
+        self.descr = nn.Sequential(
+            nn.Linear(64 * 8 * 8, 128),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.descr(x)
+        return x
 
 class HardNet(nn.Module):
     """HardNet model definition
@@ -804,7 +827,8 @@ if __name__ == '__main__':
         if not os.path.isdir(DESCS_DIR):
             os.makedirs(DESCS_DIR)
     logger, file_logger = None, None
-    model = HardNet()
+    model = TNet()
+    # model = HardNet()
     # model_weights = '/content/hardnet/pretrained/checkpoint_9.pth'
     # checkpoint = torch.load(model_weights)
     # model.load_state_dict(checkpoint['state_dict'])
