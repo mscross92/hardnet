9a7afa864ff3ce8c10bb24d58dc045776522681e
Maria Cross <mscross92@googlemail.com>
2019-10-04 10:11:30 +0100

    Update TurbidLoader.py

--- code/dataloaders/TurbidLoader.py
+++ code/dataloaders/TurbidLoader.py
@@ -93,8 +93,8 @@ class TURBID(data.Dataset):
 
                     # original patch
                     ptch = gray[int(x-0.5*s):int(x-0.5*s)+int(s),int(y-0.5*s):int(y-0.5*s)+int(s)]
-                    ptchs.append(ptch)
-                    # ptchs.append(torch.ByteTensor(np.array(ptch, dtype=np.uint8)).cuda())
+                    # ptchs.append(ptch)
+                    ptchs.append(torch.ByteTensor(np.array(ptch, dtype=np.uint8)).cuda())
                     labels.append(ll)
 
                     # perspective transform patch
@@ -116,8 +116,8 @@ class TURBID(data.Dataset):
                     M[1, 2] -= ymin
                     ptch = cv2.warpPerspective(ptch,M,(x_dif,y_dif))
                     ptch = np.array(transform(ptch))
-                    ptchs.append(ptch)
-                    # ptchs.append(torch.ByteTensor(np.array(ptch, dtype=np.uint8)).cuda())
+                    # ptchs.append(ptch)
+                    ptchs.append(torch.ByteTensor(np.array(ptch, dtype=np.uint8)).cuda())
                     labels.append(ll)
 
                     # rotated patches
@@ -125,13 +125,13 @@ class TURBID(data.Dataset):
                     M = cv2.getRotationMatrix2D((y,x), r, 1.0) # rotate about patch center
                     rotated = cv2.warpAffine(gray, M, (w, h))
                     ptch = rotated[int(x-0.5*s):int(x-0.5*s)+int(s),int(y-0.5*s):int(y-0.5*s)+int(s)]
-                    ptchs.append(ptch)
-                    # ptchs.append(torch.ByteTensor(np.array(ptch, dtype=np.uint8)).cuda())
+                    # ptchs.append(ptch)
+                    ptchs.append(torch.ByteTensor(np.array(ptch, dtype=np.uint8)).cuda())
                     labels.append(ll)
 
                     
-
         print(len(ptchs),'patches created from',ll,'locations and',nn,'images')
+        ptchs = torch.cat(ptchs, dim=1) # concat list of tensors
 
         return torch.ByteTensor(np.array(ptchs, dtype=np.uint8)), torch.LongTensor(labels)
 
